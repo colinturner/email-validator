f69dbd36fdd25dcdb221a36f7021f8dbd05e866f
Colin Turner <colinturner34@gmail.com>
2019-10-07 20:55:27 -0700

    format kickbox message in UI

--- client/src/components/KickboxResults.tsx
+++ client/src/components/KickboxResults.tsx
@@ -2,10 +2,24 @@ import React, { useState, useEffect } from "react";
 import useDebounce from "../hooks/useDebounce";
 import { isEmailValid } from "../utilities/utils";
 import { verifyEmail } from "../utilities/api";
+import { KickboxResponse } from "../interfaces/interfaces";
 
 interface Props {
   email: string | undefined;
 }
+
+const formatKickboxMessage = (kickboxResponse: KickboxResponse) => {
+  if (kickboxResponse.result === "deliverable") {
+    return "That email address is deliverable. Nice!";
+  }
+  if (kickboxResponse.did_you_mean) {
+    return `Looks like there was a problem. Did you mean to write ${kickboxResponse.did_you_mean}?`;
+  }
+  if (kickboxResponse.result === "undeliverable") {
+    return "Looks like there was a problem. Try a different email address";
+  }
+  return "";
+};
 
 const KickboxResults: React.FC<Props> = ({ email = "" }) => {
   const [isVerifying, setIsVerifying] = useState(false);
@@ -25,7 +39,11 @@ const KickboxResults: React.FC<Props> = ({ email = "" }) => {
   }, [debouncedSearchTerm]);
   return (
     <>
-      <div>{isVerifying ? "Checking email..." : kickboxResults.result}</div>
+      <div>
+        {isVerifying
+          ? "Checking email..."
+          : formatKickboxMessage(kickboxResults)}
+      </div>
     </>
   );
 };
